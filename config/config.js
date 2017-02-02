51774c243212b98271907c8d1ab6cece3a70df4e
torwood3 <torwood@hotmail.fr>
2017-02-02 21:22:41 +0100

    update docker-compose file

--- config/config.js
+++ config/config.js
@@ -2,6 +2,10 @@ var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
     env = process.env.NODE_ENV || 'development';
 
+console.log("Environnement : " + env);
+console.log("Root : " + rootPath);
+
+
 var config = {
   development: {
     root: rootPath,
